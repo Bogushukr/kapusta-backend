be669abc99b8d8a2c37c5ce0a9540f7a3a5043cc
ragimovalik <ragimovalik@gmail.com>
2021-10-09 00:40:00 +0300

    Fix on routes/api/index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,15 +1,9 @@
 const transactions = require('./transactions')
-const users = require('./users')
-const reportRouter = require('./reports')
-
-module.exports = {
-  transactions,
-  users,
-  reportsRouter,
-
+const reportsRouter = require('./reports')
 const authRouter = require('./auth')
 
 module.exports = {
   transactions,
   authRouter,
+  reportsRouter,
 }
