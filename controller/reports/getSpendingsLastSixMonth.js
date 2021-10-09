eb2cfdd9ff430cd055778ee31326d8ff1e65103f
ragimovalik <ragimovalik@gmail.com>
2021-10-09 17:30:23 +0300

    Updated last six month report controller

--- controller/reports/getSpendingsLastSixMonth.js
+++ controller/reports/getSpendingsLastSixMonth.js
@@ -2,23 +2,56 @@ const { Transaction } = require('../../model')
 const { HttpCode } = require('../../helpers/constants')
 
 const getSpendingsLastSixMonth = async (_, res) => {
+  const today = new Date()
+  const curYear = today.getFullYear().toString()
+  const curMonth = (today.getMonth() + 1).toString()
+
+  console.log(`Today is a day of ${curMonth}-${curYear}`)
+
   const pipeline = [
     {
+      $project: {
+        year: 1,
+        month: 1,
+        day: 1,
+        value: 1,
+        cashIncome: 1,
+        myNewDate: {
+          $toDate: {
+            $concat: ['$year', '-', '$month', '-', '$day'],
+          },
+        },
+      },
+    },
+    {
       $match: {
-        cashIncome: false,
+        myNewDate: {
+          $lte: new Date('2021-10'),
+        },
+        // myNewDate: {
+        //   $gte: new Date('2021-05'),
+        // },
       },
     },
     {
       $group: {
-        _id: '$month',
-        totalSum: {
+        _id: {
+          month: {
+            $month: '$myNewDate',
+          },
+          year: {
+            $year: '$myNewDate',
+          },
+        },
+        Total: {
           $sum: '$value',
         },
       },
     },
     {
       $sort: {
-        _id: -1,
+        '_id.year': -1,
+        '_id.month': -1,
       },
     },
     {
